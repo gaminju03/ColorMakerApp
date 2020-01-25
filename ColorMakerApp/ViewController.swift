5907af2f8774f72be08145cd5b1716b9fae3fe33
Juan <gamino_juan03@hotmail.com>
2020-01-25 12:58:28 -0600

    Initial Commit

---
+++ ColorMakerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorMakerApp
+//
+//  Created by Juan on 25/01/20.
+//  Copyright © 2020 usuario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
